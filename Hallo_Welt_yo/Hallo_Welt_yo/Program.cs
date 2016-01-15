506528ddd0cf1014af9e5e6ac5c92ab970c896eb
Tobias Marcus <tobtob.marcuc@web.de>
2016-01-15 12:58:04 +0100

    Hi

---
+++ Hallo_Welt_yo/Hallo_Welt_yo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hallo_Welt_yo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Welt yo!");
+        }
+    }
+}
